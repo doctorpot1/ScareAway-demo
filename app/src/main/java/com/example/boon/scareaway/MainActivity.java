e6e077e48413e86e27eb8ff3fd382aab6d8c11ff
Boon <doctorpot1@hotmail.com>
2020-05-21 06:48:52 +0800

    Added Hotspot geofencing ability to the app

--- app/src/main/java/com/example/boon/scareaway/MainActivity.java
+++ app/src/main/java/com/example/boon/scareaway/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.boon.scareaway;
 
 import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -11,7 +13,13 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.LocationServices;
+
 public class MainActivity extends AppCompatActivity {
+    private GeofencingClient geofencingClient;
+    private HotFenceManager hotFenceManager;
+    private PendingIntent geofencePendingIntent;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -25,12 +33,9 @@ public class MainActivity extends AppCompatActivity {
                     transaction.replace(R.id.fragmentContainer, gMapFragment);
                     break;
                 case R.id.navigation_upload:
+                case R.id.navigation_check_in:
                     SafeEntryFragment uploadFragment = new SafeEntryFragment();
                     transaction.replace(R.id.fragmentContainer, uploadFragment);
-                    break;
-                case R.id.navigation_check_in:
-                    SafeEntryFragment safeEntryFragment = new SafeEntryFragment();
-                    transaction.replace(R.id.fragmentContainer, safeEntryFragment);
                     break;
                 case R.id.navigation_help:
                     TraceFragment traceFragment = new TraceFragment();
@@ -46,18 +51,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        geofencingClient = LocationServices.getGeofencingClient(this);
+        hotFenceManager = new HotFenceManager(geofencingClient, getGeofencePendingIntent(), new TestHotSpots().getHotSpots());
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            hotFenceManager.CreateAllGeofence();
             navigation.setSelectedItemId(R.id.navigation_home);
-        } else { ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},GMapFragment.MY_PERMISSIONS_REQUEST_LOCATION ); }
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, GMapFragment.MY_PERMISSIONS_REQUEST_LOCATION);
+        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode != GMapFragment.MY_PERMISSIONS_REQUEST_LOCATION) { return; }
+        if (requestCode != GMapFragment.MY_PERMISSIONS_REQUEST_LOCATION) {
+            return;
+        }
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setSelectedItemId(R.id.navigation_home);
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (geofencePendingIntent != null) {
+            return geofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceBroadcastReceiver.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        geofencePendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return geofencePendingIntent;
     }
 }
