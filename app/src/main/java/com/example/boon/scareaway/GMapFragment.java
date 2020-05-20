71b060bfc2497de473eddd4a607ee1c9068ccfdf
Boon <doctorpot1@hotmail.com>
2020-05-21 00:06:11 +0800

    Added gmap fragment

--- app/src/main/java/com/example/boon/scareaway/GMapFragment.java
+++ app/src/main/java/com/example/boon/scareaway/GMapFragment.java
@@ -1,60 +1,116 @@
 package com.example.boon.scareaway;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link GMapFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link GMapFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class GMapFragment extends Fragment {
+public class GMapFragment extends Fragment implements
+        OnMapReadyCallback,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnMarkerClickListener
+{
+    private GoogleMap map;
+    private MapView mapView;
+    private boolean mapsSupported = true;
+
     public GMapFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GMapFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GMapFragment newInstance(String param1, String param2) {
-        GMapFragment fragment = new GMapFragment();
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+        initializeMap();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final FrameLayout parent = (FrameLayout) inflater.inflate(R.layout.fragment_gmap, container, false);
+        mapView = (MapView) parent.findViewById(R.id.mapView);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gmap, container, false);
+        return parent;
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        try {
+            MapsInitializer.initialize(getActivity());
+        } catch (Exception e) {
+            mapsSupported = false;
+        }
+
+        if (mapView != null) {
+            mapView.onCreate(savedInstanceState);
+        }
+        initializeMap();
     }
 
+    private void initializeMap() {
+        if (map == null && mapsSupported) mapView.getMapAsync(this);
+    }
+
+    // Callback called when Map is ready
     @Override
-    public void onDetach() {
-        super.onDetach();
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d("Map", "onMapReady()");
+        map = googleMap;
+        map.setOnMapClickListener(this);
+        map.setOnMarkerClickListener(this);
+    }
+
+    // Callback called when Map is touched
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Log.d("Map", "onMapClick("+latLng +")");
+    }
+
+    // Callback called when Marker is touched
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Log.d("Map", "onMarkerClickListener: " + marker.getPosition() );
+        return false;
     }
 }
