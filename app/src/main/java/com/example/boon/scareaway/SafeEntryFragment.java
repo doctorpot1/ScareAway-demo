beacc83e8099b63a06fc2a030d57c653596af0eb
Boon <doctorpot1@hotmail.com>
2020-05-22 02:58:32 +0800

    Created mockup prototype of Beacon SafeEntry

--- app/src/main/java/com/example/boon/scareaway/SafeEntryFragment.java
+++ app/src/main/java/com/example/boon/scareaway/SafeEntryFragment.java
@@ -2,10 +2,16 @@ package com.example.boon.scareaway;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 /**
@@ -17,6 +23,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class SafeEntryFragment extends Fragment {
+    ImageView checked;
+    Button in;
+    Button out;
+    TextView centerText;
+    TextView loadingText;
+    ProgressBar progress;
 
     public SafeEntryFragment() {
         // Required empty public constructor
@@ -45,7 +57,19 @@ public class SafeEntryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_safe_entry, container, false);
+        final ConstraintLayout parent = (ConstraintLayout)  inflater.inflate(R.layout.fragment_safe_entry, container, false) ;
+        in = (Button) parent.findViewById(R.id.checkInButton);
+        in.setOnClickListener(new View.OnClickListener() { public void onClick(View v) { checkIn(); } });
+        out = (Button) parent.findViewById(R.id.checkOutButton);
+        out.setOnClickListener(new View.OnClickListener() { public void onClick(View v) { checkOut(); } });
+        progress = (ProgressBar) parent.findViewById(R.id.progressBar2);
+        progress.setOnClickListener(new View.OnClickListener() { public void onClick(View v) { showChoice(); } });
+        loadingText = (TextView) parent.findViewById(R.id.searchText);
+        loadingText.setOnClickListener(new View.OnClickListener() { public void onClick(View v) { showChoice(); } });
+        centerText = (TextView) parent.findViewById(R.id.centerText);
+        checked = (ImageView) parent.findViewById(R.id.checkImage);
+        checked.setOnClickListener(new View.OnClickListener() { public void onClick(View v) { showChoice(); } });
+        return parent;
     }
 
     @Override
@@ -56,5 +80,32 @@ public class SafeEntryFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    private void checkIn() {
+        checked.setImageResource(R.drawable.checked_in);
+        checked.setVisibility(View.VISIBLE);
+        in.setVisibility(View.INVISIBLE);
+        out.setVisibility(View.INVISIBLE);
+        Log.e("checkIn","Checked");
+    }
+
+    private void checkOut() {
+        checked.setImageResource(R.drawable.checked_out);
+        centerText.setVisibility(View.VISIBLE);
+        checked.setVisibility(View.VISIBLE);
+        in.setVisibility(View.INVISIBLE);
+        out.setVisibility(View.INVISIBLE);
+        Log.e("checkOut","Checked");
+    }
+
+    private void showChoice() {
+        Log.e("checkOut","Checked");
+        progress.setVisibility(View.INVISIBLE);
+        loadingText.setVisibility(View.INVISIBLE);
+        checked.setVisibility(View.INVISIBLE);
+        centerText.setVisibility(View.VISIBLE);
+        in.setVisibility(View.VISIBLE);
+        out.setVisibility(View.VISIBLE);
     }
 }
